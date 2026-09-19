891e4e41db46e060fe5f447979cb2e927abdfd44
Jhansi <jhansi131@yahoo.com>
2026-09-19 14:05:59 +0200

    feat : add shell prompt

---
+++ shell_scripting/04-variables-prompt.sh
@@ -0,0 +1,9 @@
+#!/bash/bin
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+ECHO "$PERSON1:: good. What are you learning $PERSON2?"
+echo "$PERSON2:: I am practicing Shell scripting. What about you?"
+ECHO "$PERSON1:: I am learning Shell scripting. What about you?"
