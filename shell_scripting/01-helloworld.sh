1d1fcd11b47219ddbe946650511a03af664e7012
Jhansi <jhansi131@yahoo.com>
2026-09-19 13:42:03 +0200

    shell hello world

--- shell_scripting/01-helloworld.sh
+++ shell_scripting/01-helloworld.sh
@@ -2,3 +2,8 @@
 echo "hello world"
 
 echo "this is a test"
+
+echo "Suresh:: Hello Ramesh, How are you?"
+echo "Ramesh:: Hi Suresh, I am fine thanks for asking. How are you doing?"
+echo "Suresh:: doing fine. What are you learning Ramesh?"
+echo "Ramesh:: I am learning Shell. What about you?"
