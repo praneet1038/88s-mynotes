5fdada00af798e6f9adf16f6cd1b2a50dda3fca3
Jhansi <jhansi131@yahoo.com>
2026-09-20 23:49:37 +0200

    feat: check if argument is a valid number

--- shell_scripting/09-conditions.sh
+++ shell_scripting/09-conditions.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # This script will print if the given number is greater than or equal to 20 or not.
 NUMBER=$1
-if [ $NUMBER -gt 20 ]; then
+if [ -z "$NUMBER" ]; then
+    echo "Please provide a number as an argument."
+    exit 1
+elif [ $NUMBER -gt 20 ]; then
     echo "The number $NUMBER is greater than 20."
 elif [ $NUMBER -eq 20 ]; then
     echo "The number $NUMBER is equal to 20."
