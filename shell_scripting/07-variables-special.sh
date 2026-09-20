5eaf94777aad2cabdaf63fad6ca858b7e14ceb1b
Jhansi <jhansi131@yahoo.com>
2026-09-20 20:11:32 +0200

    fix: reorder echo statements.

--- shell_scripting/07-variables-special.sh
+++ shell_scripting/07-variables-special.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "This script demonstrates the use of special variables in shell scripting."
-echo "\$0 (script NAME): $0"
-echo "\$@ (all ARGUMENTS as a LIST): $@"
-echo "\$# (NUMBER of arguments): $#"
-echo "\$\$ (process ID): $$"
-echo "\$PWD (current working DIRECTORY): $PWD"
-echo "\$USER (current user): $USER"
+echo "\$0: $0 (script NAME)"
+echo "\$@: $@ (all ARGUMENTS as a LIST)"
+echo "\$#: $# (NUMBER of arguments)"
+echo "\$\$: $$ (process ID)"
+echo "\$PWD: $PWD (current working DIRECTORY)"
+echo "\$USER: $USER (current user)"
 echo "if \$USER is prints empty value, then use \$(whoami)  to get the current user. : $(whoami)"
 
 sleep 100 &
-echo "\$! (process ID of last BACKGROUND command): $!"
+echo "\$!: $! (process ID of last BACKGROUND command)"
 
-echo "\$HOME (home DIRECTORY of $USER): $HOME"
-echo "\$* (all ARGUMENTS as a SINGLE STRING): $*"
+echo "\$HOME: $HOME (home DIRECTORY of $USER)"
+echo "\$*: $* (all ARGUMENTS as a SINGLE STRING)"
