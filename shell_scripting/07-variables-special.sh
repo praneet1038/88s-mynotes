c223ec9eeaa8619feb96ba33ad2d6380d4786896
Jhansi <jhansi131@yahoo.com>
2026-09-20 18:50:36 +0200

    feat: add special characters

---
+++ shell_scripting/07-variables-special.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "This script demonstrates the use of special variables in shell scripting."
+echo "The script NAME is: $0"
+echo "All ARGUMENTS passed to the script as a LIST: $@"
+echo "The NUMBER of arguments passed to the script is: $#"
+echo "The process ID of the script is: $$"
+echo "The process ID of the last BACKGROUND command is: $!"
+sleep 100 &
+echo "Current working DIRECTORY is: $PWD"
+echo "Who am I? $USER"
+echo "Home DIRECTORY of the $USER is: $HOME"
+echo "All ARGUMENTS passed to the script as a SINGLE STRING: $*"
