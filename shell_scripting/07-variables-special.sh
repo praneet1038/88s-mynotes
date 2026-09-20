3449ae3113b0c876f3f6e0ea060a8ab93a6aeeb7
Jhansi <jhansi131@yahoo.com>
2026-09-20 18:53:51 +0200

    fix: reorder lines

--- shell_scripting/07-variables-special.sh
+++ shell_scripting/07-variables-special.sh
@@ -5,9 +5,9 @@ echo "The script NAME is: $0"
 echo "All ARGUMENTS passed to the script as a LIST: $@"
 echo "The NUMBER of arguments passed to the script is: $#"
 echo "The process ID of the script is: $$"
-echo "The process ID of the last BACKGROUND command is: $!"
-sleep 100 &
 echo "Current working DIRECTORY is: $PWD"
 echo "Who am I? $USER"
+sleep 100 &
+echo "The process ID of the last BACKGROUND command is: $!"
 echo "Home DIRECTORY of the $USER is: $HOME"
 echo "All ARGUMENTS passed to the script as a SINGLE STRING: $*"
