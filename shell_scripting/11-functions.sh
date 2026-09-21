10728ee4acb09d1b1ddad97c50cdb6e631f10eb7
Jhansi <jhansi131@yahoo.com>
2026-09-21 13:20:41 +0200

    feat: add output to log file

--- shell_scripting/11-functions.sh
+++ shell_scripting/11-functions.sh
@@ -2,29 +2,35 @@
 
 # This script will use a function to check if an installation was successful or not. It will also demonstrate the use of functions in shell scripting.
 
+LOG_FOLDER="/var/Log/shell_scripting/"
+LOG_FILE="$LOG_FOLDER$0.log"
+
 # check root user
 USER=$(id -u)
 if [ $USER -ne 0 ]; then
-    echo "This script must be run as root. Pleae run with sudo or as root user."
+    echo "This script must be run as root. Pleae run with sudo or as root user." &>> $LOG_FILE
     exit 1
 fi
 
+# create log folder if it does not exist
+mkdir -p $LOG_FOLDER
+
 check_installation() {
     if [ $1 -eq 0 ]; then
-        echo "$2 installed successfully."
+        echo "$2 installed successfully." 
     else
-        echo "$2 installation failed."
+        echo "$2 installation failed."  
         exit 1
     fi
 }
 
-dnf install nginx -y
+dnf install nginx -y &>> $LOG_FILE
 check_installation $? "Nginx"
 
-dnf install mysql -y
+dnf install mysql -y &>> $LOG_FILE
 check_installation $? "MySQL"
 
-dnf install nodejs -y
+dnf install nodejs -y &>> $LOG_FILE
 check_installation $? "Nodejs"
 
 echo "All installations completed successfully."
