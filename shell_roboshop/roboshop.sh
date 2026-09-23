835d35ba02800d88f5944582bbed493fd6267bc9
Jhansi <jhansi131@yahoo.com>
2026-09-24 01:27:39 +0200

    fix: incorrect syntax

--- shell_roboshop/roboshop.sh
+++ shell_roboshop/roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SG_ID = "sg-00e273e797ad748e9"
-AMI_ID = "ami-0220d79f3f480ecf5" # Redhat-9-DevOps-practice (joinDevOps AMI)
+SG_ID="sg-00e273e797ad748e9"
+AMI_ID="ami-0220d79f3f480ecf5" # Redhat-9-DevOps-practice (joinDevOps AMI)
 
 
 
